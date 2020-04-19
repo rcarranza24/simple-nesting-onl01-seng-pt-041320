14ebaac3867899f7390b0eeee725515620801489
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-19 23:13:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     alan_kay = alan_kay_is_known_for[:known_for]
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
