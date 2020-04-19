d2525a76519dc2491c1f2ef7c21a7721ec8fa8df
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-19 23:28:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -98,7 +98,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash[alans_new_info = "GUI"
+     programmer_hash[:alans_kay][:known_for] = "GUI"
      
      
 end
