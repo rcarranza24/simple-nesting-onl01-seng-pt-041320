8660a7204e4ad8bd9bb628994fb7a7fded0f7f9d
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-19 23:22:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,11 +58,10 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-  programmer_hash[
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  programmer_hash[:yukihiro_matsumoto] => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
 
 	programmer_hash = 
  		{
